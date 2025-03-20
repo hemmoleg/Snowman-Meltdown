ef2c121ee52b2950a3898ad93693811d0496077f
Robert <hemmoleg@gmail.com>
2025-03-20 11:05:56 +0100

    added feature to play multiple rounds / input validation for guess

--- game_logic.py
+++ game_logic.py
@@ -18,7 +18,7 @@ def display_game_state(mistakes, secret_word, guessed_letters):
         else:
             display_word += "_ "
     print("Word: ", display_word)
-    print("\n")
+    print()
 
 
 def check_game_won(secret_word, guessed_letters):
@@ -33,27 +33,57 @@ def check_game_won(secret_word, guessed_letters):
         return False
 
 
+def get_play_again():
+    while True:
+        try:
+            again = str(input("Play again? y/n: "))
+            if again != 'y' and again != 'n':
+                print("Please enter either 'n' or 'y'")
+            else:
+                break
+        except ValueError:
+            print("Please enter either 'n' or 'y'")
+
+    return True if again == 'y' else False
+
+
+def get_letter():
+    while True:
+        try:
+            guess = str(input("Guess a letter: ")).lower()
+            if len(guess) > 1:
+                print("Please enter a single letter")
+            else:
+                return guess
+        except ValueError:
+            print("Please enter a single letter")
+
+
 def play_game():
-    secret_word = get_random_word()
-    guessed_letters = []
-    mistakes = 0
+    while True:
+        secret_word = get_random_word()
+        guessed_letters = []
+        mistakes = 0
 
-    print("Welcome to Snowman Meltdown!")
-    # For now, display the initial game state.
+        print("Welcome to Snowman Meltdown!")
+        # For now, display the initial game state.
+        while mistakes < 4:
+            display_game_state(mistakes, secret_word, guessed_letters)
 
-    while mistakes < 4:
-        display_game_state(mistakes, secret_word, guessed_letters)
+            guess = get_letter()
+            print("You guessed:", guess)
+            guessed_letters.append(guess)
+            if not guess in secret_word:
+                mistakes += 1
 
-        # Prompt user for one guess (logic to be enhanced later)
-        guess = input("Guess a letter: ").lower()
-        print("You guessed:", guess)
-        guessed_letters.append(guess)
-        if not guess in secret_word:
-            mistakes += 1
+            if check_game_won(secret_word, guessed_letters):
+                print("GAME WON")
+                break
 
-        if check_game_won(secret_word, guessed_letters):
-            print("GAME WON")
+        if mistakes == 4:
+            print("GAME OVER")
+
+        if not get_play_again():
             exit()
-
-    print("GAME OVER")
-
+        else:
+            print()
