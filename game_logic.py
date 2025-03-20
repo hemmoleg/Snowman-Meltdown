cb1a2c307be618011ddff69d80683fc170368d29
Robert <hemmoleg@gmail.com>
2025-03-20 11:21:44 +0100

    restored random word selection

--- game_logic.py
+++ game_logic.py
@@ -5,8 +5,7 @@ from snowman import WORDS
 
 def get_random_word():
     """Selects a random word from the list."""
-    #return WORDS[random.randint(0, len(WORDS) - 1)]
-    return WORDS[4]
+    return WORDS[random.randint(0, len(WORDS) - 1)]
 
 
 def display_game_state(mistakes, secret_word, guessed_letters):
